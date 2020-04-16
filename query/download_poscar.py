060b49ed0f1f289d7e513f54376d34bce40900e0
Hung-Yi Wu <hywu0110@gmail.com>
2020-04-16 12:24:27 -0400

    change folder name

--- query/download_poscar.py
+++ query/download_poscar.py
@@ -9,7 +9,7 @@ from pymatgen.ext import matproj
 if __name__ == '__main__':
     # path
     query_filepath = './query_result.csv'
-    output_folderpath = './poscar_files'
+    output_folderpath = './all_poscar'
 
     # make folder
     if os.path.isdir(output_folderpath):
@@ -20,12 +20,9 @@ if __name__ == '__main__':
     matid_list = pd.read_csv(query_filepath)['material_id'].tolist()
     with matproj.MPRester(api_key='9bASScRXuQNDSebS') as m:
         for matid in tqdm.tqdm(matid_list):
-            try:
-                s = m.get_structure_by_material_id(material_id=matid,
-                        conventional_unit_cell=True)
-                p = s.to(fmt='poscar')
-                output_filepath = os.path.join(output_folderpath, '{}.poscar'.format(matid))
-                with open(output_filepath, 'w') as outfile:
-                    outfile.write(p)
-            except:
-                continue
+            s = m.get_structure_by_material_id(material_id=matid,
+                    conventional_unit_cell=True)
+            p = s.to(fmt='poscar')
+            output_filepath = os.path.join(output_folderpath, '{}.poscar'.format(matid))
+            with open(output_filepath, 'w') as outfile:
+                outfile.write(p)

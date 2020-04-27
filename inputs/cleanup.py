495ee618528c4691e984274b974b2c02b2b9da88
ZoeGOGOGO <zzy950709@gmail.com>
2020-04-27 00:53:52 -0400

    copying files to a new directory to prepare for the force field calculation after the final vasp run

--- inputs/cleanup.py
+++ inputs/cleanup.py
@@ -1,14 +1,22 @@
 import vasp_config as vc 
-import os 
+import os
+from shutil import copyfile
 
 vasp_dir_new = '/vasp_relax/'
+phonopy_inputs_dir = '/phonopy_inputs/'
+os.makedirs(os.getcwd()+phonopy_inputs_dir, exist_ok = True)
 
+master_dir = os.getcwd()
 
-v = vc.Vasp_Config()
-incar_params = v.params
-incar_params["NSW"]=1
+for dir in os.listdir(os.getcwd()+vasp_dir_new):
+    os.chdir(os.getcwd()+vasp_dir_new+dir)
+    os.makedirs(master_dir+phonopy_inputs_dir+dir, exist_ok = True)
+    
+    v = vc.Vasp_Config()
+    incar_params = v.params
+    incar_params["NSW"]=1
 
-v.INCAR_writer(incar_params,fname='/INCAR-ff')
-
-os.system("cp CONTCAR POSCAR-unit")
-v.relax_off() # turn off selective dynamics
+    v.INCAR_writer(incar_params,fname=master_dir+phonopy_inputs_dir+'INCAR-ff')
+    copyfile("CONTCAR", master_dir+phonopy_inputs_dir+"POSCAR-unit")
+    v.relax_off(master_dir+phonopy_inputs_dir+"POSCAR-unit") # turn off selective dynamics
+    

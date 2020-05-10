2d1335f5948d49a85660e76dcc2eff3083b18b0c
Malia Wenny <mwenny@g.harvard.edu>
2020-05-10 11:39:11 -0400

    changed output directory for example cleanup.py

--- inputs/cleanup.py
+++ inputs/cleanup.py
@@ -5,8 +5,8 @@ from shutil import copyfile
 import phonopy_config_generator as pcg
 
 vasp_dir_new = '/vasp_relax/'
-phonopy_inputs_dir = '/phonopy_inputs/'
-os.makedirs(os.getcwd()+phonopy_inputs_dir, exist_ok = True)
+phonopy_inputs_dir = '/n/holyscratch01/cs205/group4/example-relax/'
+os.makedirs(phonopy_inputs_dir, exist_ok = True)
 
 master_dir = os.getcwd()
 
@@ -15,17 +15,17 @@ for dir in os.listdir(os.getcwd()+vasp_dir_new):
         print("Working in directory: " + dir)
         
         os.chdir(os.getcwd()+vasp_dir_new+dir)
-        os.makedirs(master_dir+phonopy_inputs_dir+dir, exist_ok = True)
+        os.makedirs(phonopy_inputs_dir+dir, exist_ok = True)
 
         v = vc.Vasp_Config(os.getcwd()+'/config')
         incar_params = v.params
         incar_params["NSW"]=1
 
-        v.INCAR_writer(incar_params,fname="/../.."+phonopy_inputs_dir+dir+'/INCAR-ff')
-        copyfile("CONTCAR", master_dir+phonopy_inputs_dir+dir+"/POSCAR-unit")
-        v.relax_off(master_dir+phonopy_inputs_dir+dir+"/POSCAR-unit") # turn off selective dynamics
+        #v.INCAR_writer(incar_params,fname="/../.."+phonopy_inputs_dir+dir+'/INCAR-ff')
+        copyfile("CONTCAR", phonopy_inputs_dir+dir+"/POSCAR-unit")
+        v.relax_off(phonopy_inputs_dir+dir+"/POSCAR-unit") # turn off selective dynamics
         
         #create band.conf and mesh.conf for phonopy input
         p = pcg.PhonopyConfig(config_f=os.getcwd()+"/config",params_f=master_dir+'/phonopy_params.conf')
-        p.phonopy_config_writer(band=master_dir+phonopy_inputs_dir+dir+'/band.conf',mesh=master_dir+phonopy_inputs_dir+dir+'/mesh.conf')
+        p.phonopy_config_writer(band=phonopy_inputs_dir+dir+'/band.conf',mesh=phonopy_inputs_dir+dir+'/mesh.conf')
         os.chdir(master_dir)
